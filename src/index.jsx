e61b84974b0a40092d7dac87c1a4f11ccdf9ef4f
mhmd-fl <mohammed@founderandlightning.com>
2023-05-06 01:19:49 +0530

    fixing urls

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './styles/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const SERVER_URI = import.meta.env.VITE_SERVER_URI || 'http://localhost:9000';
-const SERVER_URI = 'https://seal-app-ez3bm.ondigitalocean.app/';
+const SERVER_URI = 'https://seal-app-ez3bm.ondigitalocean.app';
 
 root.render(
   <React.StrictMode>
